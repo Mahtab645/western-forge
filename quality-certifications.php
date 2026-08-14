c32fc35ac6f04f62dfc14ecc9b519b297de23e5c
Mahtab645 <ansarimehtab645@gmail.com>
2026-08-14 21:51:56 +0530

    Add new PNG images for Thai, Total, and Valero in the mfr directory
    - Added thai.png to represent Thai data.
    - Added total.png for total data representation.
    - Added valero.png for Valero data visualization.

--- quality-certifications.php
+++ quality-certifications.php
@@ -234,6 +234,102 @@ include __DIR__ . '/header.php';
     </div>
 </section>
 
+<section class="mfr-list">
+    <div class="container">
+        <p class="mfr-list-kicker">Approved</p>
+        <h2>Manufacturer List</h2>
+    </div>
+    <div class="mfr-slider" aria-label="Approved manufacturer logos">
+        <div class="mfr-track">
+            <?php
+            $manufacturers = [
+                ['file' => 'exxon.png', 'name' => 'ExxonMobil'],
+                ['file' => 'shell.png', 'name' => 'Shell'],
+                ['file' => 'marathon.png', 'name' => 'Marathon'],
+                ['file' => 'valero.png', 'name' => 'Valero'],
+                ['file' => 'phillips66.png', 'name' => 'Phillips 66'],
+                ['file' => 'chevron.png', 'name' => 'Chevron'],
+                ['file' => 'flinthills.png', 'name' => 'Flint Hills'],
+                ['file' => 'tesoro.png', 'name' => 'Tesoro'],
+                ['file' => 'sasol.png', 'name' => 'Sasol'],
+                ['file' => 'cbi.png', 'name' => 'CBI'],
+                ['file' => 'bp.png', 'name' => 'bp'],
+                ['file' => 'cimarex.png', 'name' => 'Cimarex'],
+                ['file' => 'citgo.png', 'name' => 'CITGO'],
+                ['file' => 'epic.png', 'name' => 'EPIC Piping'],
+                ['file' => 'invista.png', 'name' => 'INVISTA'],
+                ['file' => 'kbr.png', 'name' => 'KBR'],
+                ['file' => 'koch.png', 'name' => 'Koch'],
+                ['file' => 'cameron.png', 'name' => 'Cameron'],
+                ['file' => 'kiewit.png', 'name' => 'Kiewit'],
+                ['file' => 'axiall.png', 'name' => 'Axiall'],
+                ['file' => 'thai.png', 'name' => 'Thai'],
+                ['file' => 'total.png', 'name' => 'Total'],
+            ];
+            for ($loop = 0; $loop < 2; $loop++):
+                foreach ($manufacturers as $mfr):
+            ?>
+            <div class="mfr-slide">
+                <img src="<?php echo $baseUrl; ?>/images/mfr/<?php echo htmlspecialchars($mfr['file'], ENT_QUOTES, 'UTF-8'); ?>" alt="<?php echo htmlspecialchars($mfr['name'], ENT_QUOTES, 'UTF-8'); ?>">
+            </div>
+            <?php
+                endforeach;
+            endfor;
+            ?>
+        </div>
+    </div>
+</section>
+
+<section class="philosophy">
+    <div class="container">
+        <h2>Our Philosophy</h2>
+        <div class="row philosophy-grid">
+            <?php
+            $philosophyCards = [
+                [
+                    'title' => 'Safety',
+                    'lines' => ['Responsible', 'Aware', 'Vigilant'],
+                    'icon' => '<svg width="39" height="39" viewBox="0 0 10 10" fill="none" aria-hidden="true"><path d="M5.86 7.01c-.068 0-.133-.008-.198-.025l-.567-.158c-.093-.025-.283-.03-.378-.01l-.572.128c-.305.067-.658-.07-.835-.328L3.0 6.135c-.055-.08-.198-.205-.283-.25L2.17 5.612c-.278-.145-.458-.475-.428-.787l.055-.585c.01-.095-.02-.285-.057-.372L1.515 3.325c-.12-.29-.045-.657.178-.877L2.11 2.035c.068-.067.168-.23.195-.32L2.483 1.152C2.575.855 2.87.62 3.183.592l.587-.048c.095-.007.275-.067.358-.12L4.623.11c.265-.17.64-.16.897.022L6 .472c.078.055.255.125.353.138l.582.075c.31.04.593.29.673.592l.148.568c.025.092.115.26.18.33l.397.432c.213.23.268.605.133.888l-.252.53c-.043.087-.08.272-.075.37l.025.587c.015.313-.182.635-.467.765l-.535.245c-.087.04-.235.158-.295.235L6.508 6.692C6.358 6.887 6.103 7.005 5.855 7.005L5.86 7.01zM4.88 6.055c.148 0 .298.017.415.05l.568.155c.012 0 .047-.013.06-.023l.352-.462c.135-.178.378-.37.578-.46l.535-.245c0 0 .027-.037.03-.055l-.028-.582c-.01-.223.052-.528.148-.725l.252-.53c0 0-.002-.047-.012-.062L7.383 2.685c-.15-.163-.298-.438-.355-.65L6.88 1.467c0 0-.032-.035-.05-.04L6.253 1.352C6.033 1.325 5.743 1.21 5.565 1.082L5.085.742c0 0-.045-.005-.06 0L4.533 1.057C4.348 1.177 4.053 1.275 3.833 1.292l-.585.048c0 0-.04.027-.048.042L3.025 1.937c-.065.21-.227.475-.385.63L2.22 2.98c0 0-.012.045-.01.062l.223.538c.085.205.13.512.11.73l-.055.585c0 0 .02.045.032.055l.518.267c.195.1.427.308.555.488l.335.485c0 0 .05.022.06.02l.567-.128c.095-.02.208-.032.323-.032l.002.005z" fill="#F7F7F8"/><path d="M3.055 10.008c-.015 0-.027 0-.042 0-.158-.018-.285-.13-.323-.283L2.4 8.543l-1.105.492c-.145.065-.313.03-.423-.082-.11-.113-.137-.283-.067-.425L2.05 5.973l.675.328-.795 1.63.575-.255c.103-.045.218-.042.318.005.1.05.172.14.2.248l.152.625.703-1.413.672.335-1.16 2.33c-.065.128-.195.208-.335.208v-.005z" fill="#F7F7F8"/><path d="M6.945 10.008c-.14 0-.273-.08-.335-.208L5.45 7.47l.673-.335.705 1.413.152-.625c.028-.108.1-.198.2-.248.1-.05.215-.052.318.0l.575.255-.795-1.63.675-.327 1.245 2.555c.07.142.042.312-.068.425-.11.112-.277.145-.422.082L7.603 8.538l-.29 1.182c-.038.153-.168.265-.323.283-.015 0-.027 0-.042 0h-.003z" fill="#F7F7F8"/><path d="M5 5.428c-1.045 0-1.895-.85-1.895-1.895S3.955 1.638 5 1.638s1.895.85 1.895 1.895S6.045 5.428 5 5.428zm0-3.038c-.63 0-1.145.513-1.145 1.145S4.368 4.68 5 4.68s1.145-.513 1.145-1.145S5.633 2.39 5 2.39z" fill="#F7F7F8"/></svg>',
+                ],
+                [
+                    'title' => 'Quality',
+                    'lines' => ['Reliable Every Time', 'Meet or Exceed Requirements / Expectations'],
+                    'icon' => '<svg width="39" height="39" viewBox="0 0 10 10" fill="none" aria-hidden="true"><path d="M4.918 7.218H4.9c-.115-.005-.22-.063-.288-.153L2.625 4.355l.608-.445 1.71 2.333L9.435 1.133 10 1.63 5.2 7.088c-.073.082-.175.127-.282.127v.003z" fill="#F7F7F8"/><path d="M4.98 9.98C2.235 9.98 0 7.748 0 5S2.235.02 4.98.02c.923 0 1.823.252 2.603.732l-.395.643A4.21 4.21 0 0 0 4.98.772C2.65.772.753 2.67.753 5s1.897 4.228 4.228 4.228S9.208 7.33 9.208 5c0-.275-.026-.548-.078-.813l.74-.145c.06.313.093.635.093.958 0 2.748-2.235 4.98-4.98 4.98H4.98z" fill="#F7F7F8"/></svg>',
+                ],
+                [
+                    'title' => 'People',
+                    'lines' => ['Positive', 'Determined', 'Professional'],
+                    'icon' => '<svg width="39" height="39" viewBox="0 0 10 10" fill="none" aria-hidden="true"><path d="M8.002.5H1.996C1.171.5.5 1.143.5 1.933v3.841c0 .79.671 1.433 1.496 1.433h.205v1.954c0 .142.092.269.231.318a.5.5 0 0 0 .122.02c.102 0 .2-.04.269-.117L4.781 7.207h3.223c.825 0 1.496-.643 1.496-1.433V1.933C9.5 1.143 8.829.5 8.004.5h-.002zm.791 5.274c0 .418-.353.756-.79.756H4.618a.48.48 0 0 0-.268.117L2.908 8.25V6.869c0-.188-.158-.339-.354-.339H1.996c-.436 0-.79-.338-.79-.756V1.933c0-.417.354-.756.79-.756h6.006c.436 0 .789.339.789.756v3.841h.002z" fill="#F7F7F8"/><path d="M3.5 4a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM5.5 4a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM7.5 4a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" fill="#F7F7F8"/></svg>',
+                ],
+                [
+                    'title' => 'Growth',
+                    'lines' => ['Sustainable', 'Performance', 'Satisfied Customer'],
+                    'icon' => '<svg width="39" height="39" viewBox="0 0 39 39" fill="none" aria-hidden="true"><path d="M34.683 0H4.317C1.934 0 0 1.934 0 4.317v30.366C0 37.066 1.934 39 4.317 39h30.366C37.066 39 39 37.066 39 34.683V4.317C39 1.934 37.066 0 34.683 0zm1.326 34.683c0 .728-.598 1.326-1.326 1.326H4.317c-.728 0-1.326-.598-1.326-1.326V4.317c0-.728.598-1.326 1.326-1.326h30.366c.728 0 1.326.598 1.326 1.326v30.366z" fill="#F7F7F8"/><path d="M18.419 10.86V7.8H9.33c-.847 0-1.53.683-1.53 1.53v9.089h3.06v-5.396L17.337 19.5l2.163-2.163-6.477-6.477h5.396z" fill="#F7F7F8"/><path d="M28.14 25.977 21.663 19.5 19.5 21.663l6.477 6.477H20.58V31.2h9.09c.847 0 1.53-.683 1.53-1.53v-9.089H28.14v5.396z" fill="#F7F7F8"/></svg>',
+                ],
+            ];
+            foreach ($philosophyCards as $card):
+            ?>
+            <div class="col-md-6 col-lg-4">
+                <article class="philosophy-card">
+                    <span class="philosophy-card-arrow" aria-hidden="true">
+                        <svg width="22" height="22" viewBox="0 0 24 24" fill="none">
+                            <path d="M7 17 17 7M9 7h8v8" stroke="#e0393e" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </span>
+                    <div class="philosophy-card-inner">
+                        <span class="philosophy-icon"><?php echo $card['icon']; ?></span>
+                        <h3><?php echo htmlspecialchars($card['title'], ENT_QUOTES, 'UTF-8'); ?></h3>
+                        <p><?php echo implode('<br>', array_map(static function ($line) {
+                            return htmlspecialchars($line, ENT_QUOTES, 'UTF-8');
+                        }, $card['lines'])); ?></p>
+                    </div>
+                </article>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</section>
+
 <?php include __DIR__ . '/footer.php'; ?>
 </body>
 </html>
